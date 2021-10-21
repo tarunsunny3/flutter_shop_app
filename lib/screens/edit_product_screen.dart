759fc7bbd1828b35433d0d189407baa0812dbd9d
FlutLab.io <>
2021-10-21 17:47:24 +0000

    Autosave

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -8,6 +8,17 @@ class EditProductScreen extends StatefulWidget {
 
 class _EditProductScreenState extends State<EditProductScreen> {
   final _priceFocusNode = FocusNode();
+  final _descFocusNode = FocusNode();
+
+  final _imageUrlController = TextEditingController();
+  @override
+  void dispose() {
+    _priceFocusNode.dispose();
+    _descFocusNode.dispose();
+    _imageUrlController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,19 +36,36 @@ class _EditProductScreenState extends State<EditProductScreen> {
                   FocusScope.of(context).requestFocus(_priceFocusNode);
                 }),
             TextFormField(
-              decoration: InputDecoration(labelText: "Price"),
-              textInputAction: TextInputAction.next,
-              keyboardType: TextInputType.number,
-              focusNode: _priceFocusNode,
-            ),
-            TextFormField(
-                decoration: InputDecoration(labelText: "Description"),
-                maxLines: 4,
-                keyboardType: TextInputType.multiline,
+                decoration: InputDecoration(labelText: "Price"),
                 textInputAction: TextInputAction.next,
+                keyboardType: TextInputType.number,
+                focusNode: _priceFocusNode,
                 onFieldSubmitted: (_) {
-                  FocusScope.of(context).requestFocus(_priceFocusNode);
+                  FocusScope.of(context).requestFocus(_descFocusNode);
                 }),
+            TextFormField(
+              decoration: InputDecoration(labelText: "Description"),
+              maxLines: 4,
+              keyboardType: TextInputType.multiline,
+              focusNode: _descFocusNode,
+            ),
+            Row(children: [
+              Container(
+                width: 100,
+                height: 100,
+                margin: EdgeInsets.only(top: 8, right: 10),
+                decoration: BoxDecoration(
+                  border: Border.all(width: 1, color: Colors.grey),
+                ),
+                child: Container(child: _imageUrlController.text.isEmpty ? Text("Please enter a URL") : FittedBox(child: Image.network(_imageUrlController.text, fit: BoxFit.cover))),
+              ),
+              TextFormField(
+                decoration: InputDecoration(labelText: "Image URL"),
+                keyboardType: TextInputType.url,
+                textInputAction: TextInputAction.done,
+                controller: _imageUrlController,
+              ),
+            ]),
           ]),
         ),
       ),
